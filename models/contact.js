ad2959081855208c124f55884a1f1d779937c8f8
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-12-15 17:52:28 +0300

    create a contact form model that matches the frontend contact form and include the timestamps which is set to true

---
+++ models/contact.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const contactSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    phone: {
+        type: String,
+        trim: true
+    },
+    subject: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {
+    timestamps: true
+});
+
+export default mongoose.model('Contact', contactSchema);
